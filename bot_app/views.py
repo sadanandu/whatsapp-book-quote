deb67351893e7e6c9cc3be2cf65a725cff6c5564
sadanandu <sadu1437@gmail.com>
2021-04-03 18:43:11 +0530

    Fixing minor issues

--- bot_app/views.py
+++ bot_app/views.py
@@ -41,7 +41,7 @@ def index(request):
     if request.method == 'POST':
         # retrieve incoming message from POST request in lowercase
         incoming_msg = request.POST['Body'].lower()
-        came_from = request.POST['Wald']
+        came_from = request.POST['WaId']
         # create Twilio XML response
         resp = MessagingResponse()
         msg = resp.message()
@@ -78,33 +78,7 @@ You can give me the following commands:
 
 def get_quote(category_dict, category_count, came_from):
 
-#     quotes = {
-#         'money' : [
-# """“Use money to gain control over your time, because not having control of your time is such a powerful and universal drag on happiness. The ability to do what you want, when you want, with who you want, for as long as you want to, pays the highest dividend that exists in finance.”
-#     ― *Morgan Housel*""",
-# """“It's nice to have a lot of money, but you know, you don't want to keep it around forever. I prefer buying things. Otherwise, it's a little like saving sex for your old age.”
-#     ― *Warren Buffett*""",
-# """“$1,000,000 in the bank isn't the fantasy. The fantasy is the lifestyle of complete freedom it supposedly allows.”
-#     ― *Timothy Ferriss*"""],
-#
-#         'life': [
-# """“Life is what happens to us while we are making other plans.”
-#     ― *Allen Saunders*""",
-# """“Life is like riding a bicycle. To keep your balance, you must keep moving.”
-#     ― *Albert Einstein*""",
-# """“There are two basic motivating forces: fear and love. When we are afraid, we pull back from life. When we are in love, we open to all that life has to offer with passion, excitement, and acceptance. We need to learn to love ourselves first, in all our glory and our imperfections. If we cannot love ourselves, we cannot fully open to our ability to love others or our potential to create. Evolution and all hopes for a better world rest in the fearlessness and open-hearted vision of people who embrace life.”
-#     ― *John Lennon*"""],
-#
-#         'health': [
-# """“The primary reason diseases tend to run in families may be that diets tend to run in families.”
-#    ― *Michael Greger*""",
-# """“We humans have known since time immemorial something that science is only now discovering: our gut feeling is responsible in no small measure for how we feel. We are “scared shitless” or we can be “shitting ourselves” with fear. If we don’t manage to complete a job, we can’t get our “ass in gear.” We “swallow” our disappointment and need time to “digest” a defeat. A nasty comment leaves a “bad taste in our mouth.” When we fall in love, we get “butterflies in our stomach.” Our self is created in our head and our gut—no longer just in language, but increasingly also in the lab.”
-#    ― *Giulia Enders*""",
-# """“It is easy to get bogged down trying to find the optimal plan for change: the fastest way to lose weight, the best program to build muscle, the perfect idea for a side hustle. We are so focused on figuring out the best approach that we never get around to taking action. As Voltaire once wrote, “The best is the enemy of the good.”
-#    ― *James Clear*"""]
-#
-#     }
-    random.seed(came_from-time.time())
+    random.seed(int(came_from)-time.time())
     quote_row = category_dict[random.randrange(0, category_count-1)]
 
     return emoji.emojize(quote_row[0] + '\n' + ' - *{0}*, *{1}*'.format(quote_row[1], quote_row[2]))
