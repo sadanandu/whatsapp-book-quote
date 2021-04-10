7586b66b6d65dfe1fdf28c1f2fe3f11c732a68b3
sadanandu <sadu1437@gmail.com>
2021-04-10 12:22:55 +0530

    Creating first version of flask app

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from twilio.twiml.messaging_response import MessagingResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
 import emoji
 import random
 import csv
 import time
 import os
+from flask import Flask, request, Response
+
+app = Flask(__name__)
 
 curdir = os.path.dirname(__file__)
 
@@ -36,8 +37,8 @@ with open(os.path.join(curdir,'goodreads_wealth.csv'), 'r') as f:
         index += 1
     wealth_quotes = index + 1
 
-@csrf_exempt
-def index(request):
+@app.route('/bot/', methods=["GET", "POST"])
+def get_quote():
     if request.method == 'POST':
         # retrieve incoming message from POST request in lowercase
         incoming_msg = request.POST['Body'].lower()
@@ -57,24 +58,24 @@ You can give me the following commands:
 :black_small_square: *'Health'*: Health tip a day keeps doctor away! :heart:
             """, use_aliases=True)
             msg.body(response)
-            return HttpResponse(str(resp))
+            return Response(str(resp))
 
         elif incoming_msg.lower() in ['money', 'finance', 'wealth']:
             response = get_quote(wealth_dict, wealth_quotes, came_from)
             msg.body(response)
-            return HttpResponse(str(resp))
+            return Response(str(resp))
 
         elif incoming_msg.lower() in ['life', 'family']:
             response = get_quote(life_dict, life_quotes, came_from)
             msg.body(response)
-            return HttpResponse(str(resp))
+            return Response(str(resp))
 
         elif incoming_msg.lower() in ['health', 'fitness', 'body']:
             response = get_quote(health_dict, health_quotes, came_from)
             msg.body(response)
-            return HttpResponse(str(resp))
+            return Response(str(resp))
 
-    return HttpResponse(str("Hello"))
+    return Response(str("Hello"))
 
 def get_quote(category_dict, category_count, came_from):
 
@@ -82,3 +83,5 @@ def get_quote(category_dict, category_count, came_from):
     quote_row = category_dict[random.randrange(0, category_count-1)]
 
     return emoji.emojize(quote_row[0] + '\n' + ' - *{0}*, *{1}*'.format(quote_row[1], quote_row[2]))
+
+
